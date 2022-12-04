1175337a6cdfd7eb3ffab54c699c15194244e3a5
ItsFreSsh <lukes.fr@seznam.cz>
2022-12-04 16:35:03 +0100

    Added units to usage

--- src/ims.cc
+++ src/ims.cc
@@ -109,13 +109,13 @@ double calculateDragConst(double dragCoef, double crossSection, double airDensit
 
 void usage() {
     std::cout << "Usage: ./ims -a # -c # -m # [-o filename] -s # -t # -v #" << std::endl;
-    std::cout << "-a: howitzer firing angle" << std::endl;
+    std::cout << "-a: howitzer firing angle [°]" << std::endl;
     std::cout << "-c: drag coefficient" << std::endl;
-    std::cout << "-m: projectile mass" << std::endl;
+    std::cout << "-m: projectile mass [kg]" << std::endl;
     std::cout << "-o: output file name" << std::endl;
-    std::cout << "-s: projectile cross-section area" << std::endl;
-    std::cout << "-t: temperature" << std::endl;
-    std::cout << "-v: initial projectile velocity" << std::endl;
+    std::cout << "-s: projectile cross-section area [m^2]" << std::endl;
+    std::cout << "-t: temperature [°C]" << std::endl;
+    std::cout << "-v: initial projectile velocity [m/s]" << std::endl;
 }
 
 void errorUsage() {
